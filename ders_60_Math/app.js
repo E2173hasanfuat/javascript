32319f251591ccda7fe276bb695d2bf0df0d81fb
Hasan Fuat AKDAG <69896039+E2173hasanfuat@users.noreply.github.com>
2024-01-26 15:35:03 +0100

    60

---
+++ ders_60_Math/app.js
@@ -0,0 +1,12 @@
+console.log(window);
+let a = 3.55;
+console.log(Math.floor(a));//alta
+console.log(Math.ceil(a));//uste
+console.log(Math.round(a));//uste yada alta
+console.log(Math.max(1,2,3,4,50));
+console.log(Math.min(10,5));
+console.log(Math.abs(-12));
+console.log(Math.sqrt(64));
+console.log(Math.pow(5,4));
+console.log(Math.PI);
+console.log(Math.floor(Math.random()*100));
